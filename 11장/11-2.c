0aadb7ca52fcc757f5677c8a688b282ae0d517f4
KIM JEI <dev@earthquake.co.kr>
2023-11-28 13:33:06 +0900

    Create 11-2.c

---
+++ 11장/11-2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    char a[100];
+    gets(a);
+
+    int len = strlen(a);
+    int i;
+
+    for(i=0; i<len; i++){
+        if(i%2 != 0){
+        }
+        else{
+            printf("%c", a[i]);
+        }
+    }
+    
+}
